7a98a25753db6c2442c3a5da6878607eee3b95b5
himanshu-adcat <himanshus45@hdfclife.com>
2019-06-10 17:08:21 +0530

    new changes

--- main.js
+++ main.js
@@ -1,128 +1,55 @@
-;(function () {
-  var width = 900 // We will scale the photo width to this
-  var height = 0 // This will be computed based on the input stream
-  var streaming = false
-  var video = null
-  var canvas = null
-  var photo = null
-  var startbutton = null
+const video = document.getElementById('video')
+const button = document.getElementById('button')
+const select = document.getElementById('select')
+let currentStream
 
-  const button = document.getElementById('button')
-  const select = document.getElementById('select')
-
-  function gotDevices (mediaDevices) {
-    select.innerHTML = ''
-    select.appendChild(document.createElement('option'))
-    let count = 1
-    mediaDevices.forEach(mediaDevice => {
-      if (mediaDevice.kind === 'videoinput') {
-        const option = document.createElement('option')
-        option.value = mediaDevice.deviceId
-        const label = mediaDevice.label || `Camera ${count++}`
-        const textNode = document.createTextNode(label)
-        option.appendChild(textNode)
-        select.appendChild(option)
-      }
-    })
-  }
-  button.click('click', event => {
-    if (typeof currentStream !== 'undefined') {
-      stopMediaTracks(currentStream)
-    }
-    const videoConstraints = {}
-    if (select.value === '') {
-      videoConstraints.facingMode = 'environment'
-    } else {
-      videoConstraints.deviceId = { exact: select.value }
-    }
-    const constraints = {
-      video: videoConstraints,
-      audio: false
-    }
-
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then(stream => {
-        currentStream = stream
-        video.srcObject = stream
-        return navigator.mediaDevices.enumerateDevices()
-      })
-      .then(gotDevices)
-      .catch(error => {
-        console.error(error)
-      })
+function stopMediaTracks (stream) {
+  stream.getTracks().forEach(track => {
+    track.stop()
   })
+}
 
-  function startup () {
-    video = document.getElementById('video')
-    canvas = document.getElementById('canvas')
-    photo = document.getElementById('photo')
-    startbutton = document.getElementById('startbutton')
-
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: false })
-      .then(function (stream) {
-        video.srcObject = stream
-        video.play()
-      })
-
-      .catch(function (err) {
-        console.log('An error occurred: ' + err)
-      })
-
-    video.addEventListener(
-      'canplay',
-      function (ev) {
-        if (!streaming) {
-          height = video.videoHeight / (video.videoWidth / width)
-          if (isNaN(height)) {
-            height = width / (4 / 3)
-          }
-
-          video.setAttribute('width', width)
-          video.setAttribute('height', height)
-          canvas.setAttribute('width', width)
-          canvas.setAttribute('height', height)
-          streaming = true
-        }
-      },
-      false
-    )
-
-    startbutton.addEventListener(
-      'click',
-      function (ev) {
-        takepicture()
-        ev.preventDefault()
-      },
-      false
-    )
-
-    clearphoto()
-  }
-  function clearphoto () {
-    var context = canvas.getContext('2d')
-    context.fillStyle = '#AAA'
-    context.fillRect(0, 0, canvas.width, canvas.height)
-
-    var data = canvas.toDataURL('image/png')
-    photo.setAttribute('src', data)
-  }
-
-  function takepicture () {
-    var context = canvas.getContext('2d')
-    if (width && height) {
-      canvas.width = width
-      canvas.height = height
-      context.drawImage(video, 0, 0, width, height)
-
-      var data = canvas.toDataURL('image/png')
-      photo.setAttribute('src', data)
-    } else {
-      clearphoto()
+function gotDevices (mediaDevices) {
+  select.innerHTML = ''
+  select.appendChild(document.createElement('option'))
+  let count = 1
+  mediaDevices.forEach(mediaDevice => {
+    if (mediaDevice.kind === 'videoinput') {
+      const option = document.createElement('option')
+      option.value = mediaDevice.deviceId
+      const label = mediaDevice.label || `Camera ${count++}`
+      const textNode = document.createTextNode(label)
+      option.appendChild(textNode)
+      select.appendChild(option)
     }
-  }
+  })
+}
 
-  window.addEventListener('load', startup, false)
-  navigator.mediaDevices.enumerateDevices().then(gotDevices)
-})()
+button.addEventListener('click', event => {
+  if (typeof currentStream !== 'undefined') {
+    stopMediaTracks(currentStream)
+  }
+  const videoConstraints = {}
+  if (select.value === '') {
+    videoConstraints.facingMode = 'environment'
+  } else {
+    videoConstraints.deviceId = { exact: select.value }
+  }
+  const constraints = {
+    video: videoConstraints,
+    audio: false
+  }
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(stream => {
+      currentStream = stream
+      video.srcObject = stream
+      return navigator.mediaDevices.enumerateDevices()
+    })
+    .then(gotDevices)
+    .catch(error => {
+      console.error(error)
+    })
+})
+
+navigator.mediaDevices.enumerateDevices().then(gotDevices)
